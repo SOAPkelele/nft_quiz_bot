ceb1c035289bebfdaa43f68a1e7fc9a8f131ea3a
soapkelele <buev_anton@mail.ru>
2021-12-13 00:37:52 +0300

    upd

--- app/handlers/menu/main_menu.py
+++ app/handlers/menu/main_menu.py
@@ -9,36 +9,6 @@ from app.utils import delete_message
 
 gettext = i18n.gettext
 
-about_us = gettext("Они пишут о нас")
-local_trade_info = f"{about_us}:\n\n" \
-                   "<b>ENTREPRENEUR</b>\n" \
-                   "https://www.entrepreneur.com/article/388049\n\n" \
-                   "<b>FORBES</b>\n" \
-                   "https://www.forbes.fr/business/simplifier-le-monde-de-la-finance-decentralisee-le-nouveau-management-de-localtrade/\n\n" \
-                   "<b>YAHOO</b>\n" \
-                   "https://finance.yahoo.com/news/localtrade-exchange-shares-latest-strategic-175000265.html?guccounter=1\n\n" \
-                   "<b>INVESTING</b>\n" \
-                   "https://www.investing.com/news/cryptocurrency-news/how-a-crypto-exchange-platform-is-going-global-the-rise-of-localtrade-2591587\n\n" \
-                   "<b>VC</b>\n" \
-                   "https://vc.ru/u/185546-ikrom-ergashev/290483-mozhet-li-novichok-zarabotat-v-defi-kak-osvoit-6-pribylnyh-instrumentov-dohodnogo-fermerstva\n\n" \
-                   "<b>NEWSBTC</b>\n" \
-                   "https://www.newsbtc.com/news/company/can-decentralized-finance-become-the-futures-new-digital-economy/\n\n" \
-                   "<b>BTCMANAGER</b>\n" \
-                   "https://btcmanager.com/localtrades-literacy-crypto-defi/\n\n" \
-                   "🌟 Our media 🌟\n" \
-                   "Exchange Website - https://localtrade.cc/\n" \
-                   "DeFi Lab Website - https://lab.localtrade.cc/\n" \
-                   "Community - https://t.me/lt_community\n" \
-                   "Telegram Channel - https://t.me/localtradecc\n" \
-                   "Twitter - https://twitter.com/LocaltradeC\n" \
-                   "Facebook - https://www.facebook.com/localtrade.cc\n" \
-                   "Instagram - https://www.instagram.com/localtrade.cc\n" \
-                   "LinkedIn - https://www.linkedin.com/company/local-trade/\n" \
-                   "Medium - https://blog.localtrade.cc/\n" \
-                   "Wiki - https://docs.localtrade.cc/\n" \
-                   "YouTube - https://www.youtube.com/CryptoTalkz\n" \
-                   "Help center - https://localtradesupport.zendesk.com/"
-
 
 async def menu_handler(call: types.CallbackQuery):
     logger.info(f"User [{call.from_user.id}] requested menu")
@@ -49,7 +19,38 @@ async def menu_handler(call: types.CallbackQuery):
 
 async def learn_more_handler(call: types.CallbackQuery):
     logger.info(f"User [{call.from_user.id}] wants to learn more about LocalTrade")
-    await call.message.edit_text(gettext(local_trade_info),
+
+    about_us = gettext("Они пишут о нас")
+    local_trade_info = f"{about_us}:\n\n" \
+                       "<b>ENTREPRENEUR</b>\n" \
+                       "https://www.entrepreneur.com/article/388049\n\n" \
+                       "<b>FORBES</b>\n" \
+                       "https://www.forbes.fr/business/simplifier-le-monde-de-la-finance-decentralisee-le-nouveau-management-de-localtrade/\n\n" \
+                       "<b>YAHOO</b>\n" \
+                       "https://finance.yahoo.com/news/localtrade-exchange-shares-latest-strategic-175000265.html?guccounter=1\n\n" \
+                       "<b>INVESTING</b>\n" \
+                       "https://www.investing.com/news/cryptocurrency-news/how-a-crypto-exchange-platform-is-going-global-the-rise-of-localtrade-2591587\n\n" \
+                       "<b>VC</b>\n" \
+                       "https://vc.ru/u/185546-ikrom-ergashev/290483-mozhet-li-novichok-zarabotat-v-defi-kak-osvoit-6-pribylnyh-instrumentov-dohodnogo-fermerstva\n\n" \
+                       "<b>NEWSBTC</b>\n" \
+                       "https://www.newsbtc.com/news/company/can-decentralized-finance-become-the-futures-new-digital-economy/\n\n" \
+                       "<b>BTCMANAGER</b>\n" \
+                       "https://btcmanager.com/localtrades-literacy-crypto-defi/\n\n" \
+                       "🌟 Our media 🌟\n" \
+                       "Exchange Website - https://localtrade.cc/\n" \
+                       "DeFi Lab Website - https://lab.localtrade.cc/\n" \
+                       "Community - https://t.me/lt_community\n" \
+                       "Telegram Channel - https://t.me/localtradecc\n" \
+                       "Twitter - https://twitter.com/LocaltradeC\n" \
+                       "Facebook - https://www.facebook.com/localtrade.cc\n" \
+                       "Instagram - https://www.instagram.com/localtrade.cc\n" \
+                       "LinkedIn - https://www.linkedin.com/company/local-trade/\n" \
+                       "Medium - https://blog.localtrade.cc/\n" \
+                       "Wiki - https://docs.localtrade.cc/\n" \
+                       "YouTube - https://www.youtube.com/CryptoTalkz\n" \
+                       "Help center - https://localtradesupport.zendesk.com/"
+
+    await call.message.edit_text(local_trade_info,
                                  reply_markup=MenuKb().back_to_menu(gettext),
                                  disable_web_page_preview=True)
 
--- app/utils/send_score_photo.py
+++ app/utils/send_score_photo.py
@@ -10,8 +10,14 @@ def score_photo(text: str, chat_id: int):
     image = Image.open(background)
     w_image, h_image = image.size
     draw = ImageDraw.Draw(image)
+
     font = ImageFont.truetype(str(font_file), size=95)
     w_text, h_text = font.getsize_multiline(text)
+
+    if w_text > w_image:
+        font = ImageFont.truetype(str(font_file), size=75)
+        w_text, h_text = font.getsize_multiline(text)
+
     draw.multiline_text(xy=((w_image - w_text) / 2, (h_image - h_text) / 2),
                         text=text,
                         fill="black",
